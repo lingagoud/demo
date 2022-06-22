c513daa4e0fb67dfcab4101b7a4f905317a851b1
lingagoud <lingagoud1279@gmail.com>
2022-06-22 11:41:18 +0530

    basic practice

---
+++ practice1.js
@@ -0,0 +1,13 @@
+//document.write("its working");
+
+//function validate(){
+//    
+//    alert(y);
+    
+
+function colorchange(){
+   var x= Document.getElementById("li");
+   x.style.color="red";
+}
+
+
